310eebbc2b2a7915373295b8d6d1f4f7255b9ba5
André Gomes <andre.gomes@nfon.com>
2026-01-15 12:29:31 +0000

    revert last commit

--- server/handler.go
+++ server/handler.go
@@ -326,9 +326,9 @@ func (h *Handler) startCallWithAgent(phone string) {
 
 	// Build Call API request
 	callReq := callapi.MakeCallRequest{
-		Extension:     botExtension,
-		Caller:        "351918253235",
-		CallerContext: "global",
+		Extension:     agent.Extension,
+		Caller:        agent.ExternalNr,
+		CallerContext: agent.Customer,
 		Callee:        h.callAPIClient.BOT,
 		CalleeContext: "global",
 	}
